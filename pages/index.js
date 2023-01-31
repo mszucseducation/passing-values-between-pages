74a53d1083b5e2b75ed541f3b4295049e10dd863
Monika Szucs <mszucs.education@gmail.com>
2023-01-31 15:23:27 -0800

    changed styles

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '@/styles/Home.module.css'
 import ParentToChild from './ParentOne.js'
 import ChildToParent from './ParentTwo.js'
 
